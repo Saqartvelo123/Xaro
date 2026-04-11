84de81d503ea1941163d5e41aa1c621a56addffd
Saqartvelo123 <kharabadzegiorgi69@gmail.com>
2026-04-11 18:01:15 +0200

    გავასწორე პორტის კონფიგურაცია Render-ისთვის

--- app.py
+++ app.py
@@ -53,4 +53,7 @@ def register():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    import os
+    # Render ავტომატურად აწვდის პორტს PORT ცვლადით
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
